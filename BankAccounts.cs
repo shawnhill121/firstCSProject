13ee0f7cc50a34c701f8bb4cb36054d18c89b54b
Shawn Hill <shawn.hill121@gmail.com>
2018-02-12 20:12:24 -0500

    adds project files

---
+++ BankAccounts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace classes
+{ 
+    public class BankAccount
+    {
+	    public string Number { get; }
+
+        public string Owner { get; set; }
+
+        public decimal Balance { get; }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        private static int accountNumberSeed = 1234567890;
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+    }
+}
